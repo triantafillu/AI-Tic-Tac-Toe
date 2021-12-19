a3d0ca2eb9cc8434990c4b753f1758645c7df3b3
Aleksandra Triantafillu <alexandra00219@gmail.com>
2021-12-19 17:24:51 +0100

    added translation from base 3 to decimal and back

---
+++ main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+
+// Translate the table represented by a number of base 3 to the base 10
+uint32_t translate10 (uint32_t table)
+{
+    uint32_t digit;
+    uint32_t res = 0;
+    uint32_t power = 0;
+
+    // Iterate over the table
+    while(table != 0)
+    {
+        // Pick up the last digit of the number
+        digit = table % 10;
+
+        // + digit multiplied by a 3 in power corresponding to position in table
+        res += digit * pow(3,power);
+
+        // Increment power
+        power++;
+
+        // Remove the last digit
+        table = table / 10;
+    }
+
+    return res;
+}
+
+// Translate the decimal number to its table representation (base 3)
+uint32_t translate3(uint32_t number)
+{
+    uint32_t res = 0;
+    uint32_t power = 0;
+    uint32_t rem;
+
+    while (number != 0)
+    {
+        // Pick up the remainder
+        rem = number % 3;
+
+        // Get the result of division by base (3)
+        number = number / 3;
+
+        // The remainder becomes the lowest digit
+        res += rem * pow(10, power);
+
+        // Increment the power
+        power++;
+    }
+
+    return res;
+}
+
+int main() {
+    translate3( 15774);
+    // 210122020
+    return 0;
+}
