9633e21704b6da4f727139b8c91e1eebf502adeb
Yanis <houriyanis.20@gmail.com>
2021-12-24 22:38:06 +0100

    Init boite, rand bille

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <math.h>
 #include "aide_projet.c"
 #include "arraylist.c"
+#include<time.h>
 
 // Returns 1 if 3 in a column of value "val"
 _Bool isColumn(uint8_t table[3][3], uint32_t val)
@@ -152,6 +153,49 @@ uint32_t translate3(uint32_t number)
     }
 
     return res;
+}
+
+//Fonction qui initialise un boite: 
+//Une boite se compose de 9 boules de couleurs différentes 
+//Et on a un nombre en base 3 correspondant au jeu de la carte 
+configuration* init_box(uint32_t g){
+    configuration* res= malloc(sizeof(configuration)); 
+    res->game=g; 
+    //on initialise l'arraylist 
+    maillon* boules=malloc(sizeof(boules)*9); 
+    for(int i=0; i<8; i++){
+        boules[i].bille=i; //couleur de la bille 
+        boules[i].next=&(boules[i+1]); 
+    } 
+    //Boules non jouées
+    boules[8].bille=8; //8 <=> pink 
+    boules[8].next=NULL; 
+    res->free->head=&(boules[0]); 
+    res->free->tail=&(boules[8]); 
+    res->free->size=9; 
+    //Boules jouées
+    res->taken->head=NULL;
+    res->taken->tail=NULL;
+    res->taken->size=0; 
+
+    return res; 
+}
+
+//Fonction qui renvoie une bille au hasard dans une boite 
+enum billes rand_bille(configuration* boite){
+    srand(time(NULL)); 
+    int nb=rand()%(boite->free->size); 
+    int i=0; 
+    maillon* m=boite->free->head; 
+    while(i<nb && m!=NULL){
+        m=m->next; 
+        i++; 
+    }
+    if(m!=NULL){
+        return m->bille; 
+    }
+    else    
+        return -1; 
 }
 
 int main() {
